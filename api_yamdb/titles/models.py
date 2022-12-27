bcd06876f692388538c110329c279239b8786931
Maksim Kautarov <just5py@yandex.ru>
2022-12-27 20:28:42 +0300

    added whitespaces in models.py, set null=True if we need on_delete=SET_NULL. Added TEMPLATES_DIR in list of DIRS in TEMPLATES

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -6,49 +6,50 @@ User = get_user_model()
 
 
 class Category(models.Model):
-    name=models.CharField(max_length=16)
-    slug=models.SlugField(unique=True)
+    name = models.CharField(max_length=16)
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Genre(models.Model):
-    name=models.CharField(max_length=16)
-    slug=models.SlugField(unique=True)
+    name = models.CharField(max_length=16)
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Title(models.Model):
-    name=models.CharField(max_length=48)
-    year=models.IntegerField()
-    category=models.ForeignKey(
+    name = models.CharField(max_length=48)
+    year = models.IntegerField()
+    category = models.ForeignKey(
         Category,
         related_name='titles',
+        null=True,
         on_delete=models.SET_NULL
     )
-    genre=models.ManyToManyField(Genre, through='GenreTitle')
+    genre = models.ManyToManyField(Genre, through='GenreTitle')
 
     def __str__(self):
         return self.name
 
 
 class Review(models.Model):
-    title_id=models.ForeignKey(
+    title_id = models.ForeignKey(
         Title,
         related_name='reviews',
         on_delete=models.CASCADE
     )
-    text=models.TextField()
-    author=models.ForeignKey(
+    text = models.TextField()
+    author = models.ForeignKey(
         User,
         related_name='reviews',
         on_delete=models.CASCADE
     )
-    score=models.IntegerField()
-    pub_date=models.DateTimeField(
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(
         auto_now_add=True,
         db_index=True
     )
@@ -58,18 +59,18 @@ class Review(models.Model):
 
 
 class Comment(models.Model):
-    review_id=models.ForeignKey(
+    review_id = models.ForeignKey(
         Review,
         related_name='comments',
         on_delete=models.CASCADE
     )
-    text=models.TextField
-    author=models.ForeignKey(
+    text = models.TextField
+    author = models.ForeignKey(
         User,
         related_name='comments',
         on_delete=models.CASCADE
     )
-    pub_date=models.DateTimeField(
+    pub_date = models.DateTimeField(
         auto_now_add=True,
         db_index=True
     )
@@ -79,11 +80,11 @@ class Comment(models.Model):
 
 
 class GenreTitle(models.Model):
-    title_id=models.ForeignKey(
+    title_id = models.ForeignKey(
         Title,
         on_delete=models.CASCADE
     )
-    genre_id=models.ForeignKey(
+    genre_id = models.ForeignKey(
         Genre,
         on_delete=models.CASCADE
     )
