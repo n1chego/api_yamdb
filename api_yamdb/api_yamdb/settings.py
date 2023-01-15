bc3f33f920e9fdf973cef072f84609eceb96aeee
Сергей Ципичев <n1chego@mail.ru>
2023-01-15 18:08:21 +0300

    simplejwt deleted from installed apps

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
     'django_filters',
     'api',
     'reviews',
